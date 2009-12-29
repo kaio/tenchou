9f9770977792f5afb40cf03042bfcd24b4f5833f
Caius 'kaio' Chance <cchance@strauss.(none)>
2009-12-30 05:45:53 +1000

    - Started versioning. - Fixed GPL messages broke 'package' on .java files and root tag of .xml files. - Exported first apk file.

--- src/me/kaio/tenchou/Main.java
+++ src/me/kaio/tenchou/Main.java
@@ -1,24 +1,24 @@
-/*
-* tenchou - Mobile Data Mining Tools for Shop Managers.
-*
-* Copyright (c) 2009 Caius 'kaio' Chance <k at kaio.me>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2, or (at your option)
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
-
 package me.kaio.tenchou;
+
+/*
+ * tenchou - Mobile Data Mining Tools for Shop Managers.
+ *
+ * Copyright (c) 2009 Caius 'kaio' Chance <k at kaio.me>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -64,7 +64,7 @@ public class Main extends Activity {
 	}
 
 	private void calculate() {
-		
+
 		// compute gross-profit-per-customer
 		String grossProfitPerCustomerOutput, grossProfitPerItemOutput;
 		try {
